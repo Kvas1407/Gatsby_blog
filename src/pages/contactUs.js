584f3657656f90acdad1a54e1df1520f2d351ad4
keerthivasan <77402472+Kvas1407@users.noreply.github.com>
2021-08-22 22:30:25 -0400

    Created a Contact page

--- src/pages/contactUs.js
+++ src/pages/contactUs.js
@@ -1,7 +1,14 @@
 import React from "react"
 
 const contact = () => {
-    return <div>Contact Details</div>
+    return (
+        <>
+    <h1>Contact Details</h1>
+    <p>Mobile: 226(998)-6506</p>
+    <p>Mail: Keerthik1407@gmail.com</p>
+       </>  
+    )
+    
 }
 
 export default contact
